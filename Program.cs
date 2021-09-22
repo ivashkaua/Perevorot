f585775bd69e5f3b1ca6ecce3a8ba6e7778e95d9
ivashkaua <86633081+ivashkaua@users.noreply.github.com>
2021-09-22 19:45:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Perevorot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива: \t");
+            int ArrNumber = int.Parse(Console.ReadLine());
+            int[] array = new int[ArrNumber];
+
+            for (int l = 0; l < ArrNumber; l++)
+            {
+                Console.Write($"\nВведите число с индексом {l} \t");
+                array[l] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write($"\nТаким образом, массив выглядит следующим образом:\n\n");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.Write($"\nИ теперь наоборот:\n\n");
+            for (int i = array.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
